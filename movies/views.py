aaa208f8aea016a901c814a8423e969e0cc9952a
Antonio Garcia <antonio.garcia@hvex.com.br>
2022-03-01 21:18:23 -0300

    feat: adiciona arquivos da view de movies

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+from .models import Movie
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('hello world')
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
